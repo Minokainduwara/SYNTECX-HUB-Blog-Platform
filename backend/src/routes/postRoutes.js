4677e522f34b18947ac03a45057b56cdf756040f
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-04-29 11:51:19 +0530

    Feat: Post Routes

---
+++ backend/src/routes/postRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+import {
+  createPost,
+  getPosts,
+  getPostById,
+  updatePost,
+  deletePost
+} from "../controllers/postController.js";
+
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/", getPosts);
+router.get("/:id", getPostById);
+
+router.post("/", protect, createPost);
+
+router.put("/:id", protect, updatePost);
+
+router.delete("/:id", protect, deletePost);
+
+export default router;
